204bd418f01fb93e196aab77130aac6e676a38db
Dinesh_Gelam <gsdinesh1998@gmail.com>
2024-04-07 22:31:33 -0400

    a star implementation

--- proj3p2_SaiDinesh_gowtam.py
+++ proj3p2_SaiDinesh_gowtam.py
@@ -6,7 +6,7 @@ import json
 import cv2
 from queue import PriorityQueue
 from matplotlib import pyplot as plt
-
+import time
 
 
 class PossibleNode :
@@ -155,8 +155,15 @@ def move(node, action):
     print("............................................")
     # angle = 0
     angle = (r / L) * (action[0] - action[1]) * dt
+    # angle = angle*180/3.14
     
     new_angle = node.get_angle() + angle
+    # new_angle = new_angle % 6.28
+    # print(new_angle)
+    # print(angle)
+    # print("............................................")
+    # time.sleep(0.1)
+
     dx = r/2*(action[0]+action[1])*math.cos((new_angle))*dt 
     dy = r/2*(action[0]+action[1])*math.sin((new_angle))*dt
     vx = r/2*(action[0]+action[1])*math.cos(math.radians(new_angle))
@@ -192,6 +199,125 @@ rpm2 = rpm2 * ((2*np.pi)/60)
 action_list = [[0,rpm1],[rpm1,0],[rpm1,rpm1],[0,rpm2],[rpm2,0],[rpm2,rpm2],[rpm1,rpm2],[rpm2,rpm1]]
 
 
+def a_star_implementation(first_point, end_point, first_orentation):
+    global cmans_vel
+    """
+    Implements the A* algorithm to find the optimal path from the start point to the goal point.
+
+    Parameters:
+    - first_point (tuple): The (x, y) coordinates of the start point.
+    - end_point (tuple): The (x, y) coordinates of the goal point.
+    - first_orentation (int): The orientation of the robot at the start point.
+    """
+    # Create a priority queue to store the nodes to be explored
+    list = PriorityQueue()
+    # print(list)
+
+    # Create a dictionary to store the visited nodes
+    visited = dict()
+    
+
+    # Create a set to store the fixed nodes
+    fixed_list = set()
+
+    # Create the first node with the start point, orientation, and initial cost
+    first_node = PossibleNode(first_point[0], first_point[1], first_orentation, None, 0, distance(first_point, end_point))
+
+    # Add the first node to the priority queue, visited dictionary, and fixed list
+    list.put((first_node.get_total_cost(), first_node))
+    visited[(first_point[0], first_point[1])] = first_node
+    fixed_list.add(first_point)
+    count = 0
+    while not list.empty():
+        count += 1
+        # Get the node with the lowest cost from the priority queue
+        this_node = list.get()[1]
+        this_point = (this_node.x, this_node.y)
+        # print(this_point)
+        goal_x, goal_y = end_point[0], end_point[1]
+        # print(goal_x, goal_y)
+        
+        # Check if the current node is close to the goal point
+        if math.sqrt((this_point[0] - goal_x) ** 2 + (this_point[1] - goal_y) ** 2) <= 100:
+            # Perform backtracking to find the optimal path
+            print("Goal Reached")
+            backtrackPath = []
+            vel = []
+            ang = []
+            while this_node is not None:
+                backtrackPath.append((this_node.x , this_node.y, this_node.get_angle()))
+                # frame[node.y, node.x] = (173, 46, 0)
+                this_node = this_node.get_parent_node()
+            backtrackPath.reverse()
+            # print(backtrackPath)
+            an =0
+
+            for i in range(len(backtrackPath)-1):
+                cv2.arrowedLine(frame, (backtrackPath[i][0], backtrackPath[i][1]), (backtrackPath[i + 1][0], backtrackPath[i + 1][1]), (0, 0, 255), 10, tipLength=0.1)
+                x_velocity = (backtrackPath[i+1][0] - backtrackPath[i][0])/3
+                y_velocity = (backtrackPath[i+1][1] - backtrackPath[i][1])/3
+                velocity = math.sqrt(x_velocity**2 + y_velocity**2)/1000
+                # angular_velocity = (backtrackPath[i+1][2] - backtrackPath[i][2])/dt
+                angular_velocity = (math.atan(y_velocity/x_velocity)-an)/3
+                an = math.atan(y_velocity/x_velocity)
+                vel.append(0)
+                vel.append(velocity)
+                ang.append(-1*angular_velocity)
+                ang.append(0)
+                # print("Velocity: ", velocity)
+                # print("Angular Velocity: ", angular_velocity)
+                # # time.sleep(dt)
+                frame_resized = cv2.resize(frame, (screen_width, screen_height))
+                cv2.imshow("frame1",frame_resized)
+                cv2.waitKey(10)
+                # print(list)
+                # writer_video.write(frame)
+            cv2.waitKey(5000)
+            return vel , ang
+
+            
+        action_list = [[0,rpm1],[rpm1,0],[rpm1,rpm1],[0,rpm2],[rpm2,0],[rpm2,rpm2],[rpm1,rpm2],[rpm2,rpm1]]
+        # print(action_list)
+       
+        for action in action_list:
+            this_node_ = this_node
+            x = 1
+            while x>0.1:
+                possible_next_node = move(this_node_, action)
+               
+                #    print(possible_next_node)
+                if possible_next_node is not None:
+                            this_node = possible_next_node
+                            new_coordinate = possible_next_node.get_points()
+                            # print(new_coordinate)
+                            if new_coordinate not in fixed_list:
+                                if new_coordinate not in visited:
+                                    # Add the possible next node to the priority queue, visited dictionary, and fixed list
+                                    list.put((possible_next_node.get_total_cost(), possible_next_node))
+                                    visited[new_coordinate] = possible_next_node
+                                    fixed_list.add(new_coordinate)
+                                    
+                                    if possible_next_node.get_parent_node() is not None:
+                                        cv2.line(frame, possible_next_node.get_points(), possible_next_node.get_parent_node().get_points(), (255, 0, 0), 5)
+                                        if count %100 == 0:
+                                            writer_video.write(frame)
+                                            frame_resized = cv2.resize(frame, (screen_width, screen_height))
+                                            cv2.imshow("frame1",frame_resized)
+                                            cv2.waitKey(10)
+                                            writer_video.write(frame_resized)
+                                        
+                                            # print(list)
+                                    
+                                        
+                                else:
+                                    if visited[new_coordinate].get_total_cost() > possible_next_node.get_total_cost():
+                                        # Update the visited node with a lower cost and add it to the priority queue
+                                        visited[new_coordinate] = possible_next_node
+                                        list.put((possible_next_node.get_total_cost(), possible_next_node))
+                            
+  
+                x= x-0.1
+
 
 
 
@@ -200,4 +326,5 @@ writer_video = cv2.VideoWriter('GD.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 1000, 
 
 if __name__ == "__main__":
     start_point, end_point, angle, goal_angle = inital_final_goals()
+    vel,ang = a_star_implementation(start_point, end_point, angle)
     writer_video.release()
